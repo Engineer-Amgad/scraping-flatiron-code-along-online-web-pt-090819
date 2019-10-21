9f6f418cde8cd0143e8cbca25923ffb7ceb8f55c
Amgad Malik <amgadmalik@yahoo.com>
2019-10-21 01:16:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,9 +15,12 @@ class Scraper
     #   course.title = post.css("h2").text
     #   course.schedule = post.css(".date").text
     #   course.description = post.css("p").text
-    end
+    #end
   end
   
+  def get_courses
+    
+  end 
   def print_courses
     self.make_courses
     Course.all.each do |course|
